92b498be5f233878b22fd2323300ac10cc016bdc
Marco Antonio Gutierrez Lopez <itonar@hotmail.com>
2018-05-15 11:17:57 -0700

    Initial Commit

---
+++ ChatBubbles/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChatBubbles
+//
+//  Created by Marco Antonio Gutierrez López on 5/15/18.
+//  Copyright © 2018 N Soluciones Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
